473f92aed6608d2e6c8c917b8eec17a92ab872b9
walker <zhangheshan@funlink-tech.com>
2020-11-10 13:51:25 +0800

    [GL][添加Toast]

--- Example/ios_base_foundation/AppDelegate.swift
+++ Example/ios_base_foundation/AppDelegate.swift
@@ -15,7 +15,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        self.window = UIWindow.init(frame: UIScreen.main.bounds)
+        self.window?.backgroundColor = UIColor.white
+        self.window?.rootViewController = ViewController()
+        self.window?.makeKeyAndVisible()
+        
         return true
     }
 
--- Example/ios_base_foundation/ViewController.swift
+++ Example/ios_base_foundation/ViewController.swift
@@ -1,24 +1,34 @@
 //
 //  ViewController.swift
-//  ios_base_foundation
+//  ios_base_foundation_Example
 //
-//  Created by tliens on 11/09/2020.
-//  Copyright (c) 2020 tliens. All rights reserved.
+//  Created by walker on 2020/11/10.
+//  Copyright © 2020 CocoaPods. All rights reserved.
 //
 
 import UIKit
+import ios_base_foundation
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        self.view.backgroundColor = UIColor.white
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func ct_showToast(_ sender: UIButton) {
+        
+        CTToast.ct_showPositionToast(baseView: nil, message: "这是一个toast", title: nil, image: nil, completion: nil)
     }
-
+    
+    
+    @IBAction func ct_showIndicator(_ sender: UIButton) {
+        // 指示器
+        CTToast.ct_showIndicator(baseView: nil, preventUserAction: true, position: .center)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            CTToast.ct_hideIndicator(baseView: nil)
+        }
+    }
 }
-
